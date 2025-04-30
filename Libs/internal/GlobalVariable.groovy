18b6b5547146a09ba43c1aa6f22bb1f122086755
JanBeningNorima <jan.bening@Norimaconsulting.com>
2025-04-30 18:49:20 +0800

    Initial Commit

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -14,7 +14,6 @@ public class GlobalVariable {
         try {
             def selectedVariables = TestCaseMain.getGlobalVariables("default")
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
-            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
     
             
         } catch (Exception e) {
