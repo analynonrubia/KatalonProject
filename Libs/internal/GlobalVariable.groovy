4a9c5611b1ad66419b2a8a0ccdba409f82d3a20b
JanBeningNorima <jan.bening@Norimaconsulting.com>
2025-05-09 15:24:21 +0800

    Added GlobalVariables class and groovy

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -14,6 +14,7 @@ public class GlobalVariable {
         try {
             def selectedVariables = TestCaseMain.getGlobalVariables("default")
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
     
             
         } catch (Exception e) {
