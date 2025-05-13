ed125be18fb11a373b0becbd9c4b8c625403319d
JanBeningNorima <jan.bening@Norimaconsulting.com>
2025-05-13 10:52:43 +0800

    Added : New Environment File - Dev Updated Test Case Login with Encrypted Text Update git ignore files

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -9,6 +9,18 @@ import com.kms.katalon.core.main.TestCaseMain
  */
 public class GlobalVariable {
      
+    /**
+     * <p>Profile Dev : gw user name
+Profile QA : applegate</p>
+     */
+    public static Object userName
+     
+    /**
+     * <p>Profile Dev : gw password
+Profile QA : guidewire password</p>
+     */
+    public static Object password
+     
 
     static {
         try {
@@ -16,6 +28,8 @@ public class GlobalVariable {
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
             selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
     
+            userName = selectedVariables['userName']
+            password = selectedVariables['password']
             
         } catch (Exception e) {
             TestCaseMain.logGlobalVariableError(e)
