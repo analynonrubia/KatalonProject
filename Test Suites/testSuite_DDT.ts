4cb280fe4fb239311149a28173bcba26fddefa6b
JanBeningNorima <jan.bening@Norimaconsulting.com>
2025-05-14 08:24:08 +0800

    Update testSuite_DDT.ts file

--- Test Suites/testSuite_DDT.ts
+++ Test Suites/testSuite_DDT.ts
@@ -16,17 +16,26 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/test_ddt</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>6f495804-e744-49cb-b239-f1aa5070fd39</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Accounts/Person Account</testDataId>
+      </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>6f495804-e744-49cb-b239-f1aa5070fd39</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>First Name</value>
          <variableId>0149925f-de45-4b4c-97f0-02b89fe61f66</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>6f495804-e744-49cb-b239-f1aa5070fd39</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Last Name</value>
          <variableId>c5f1bd3f-45d8-4926-aa33-350f7d4e47d2</variableId>
       </variableLink>
    </testCaseLink>
